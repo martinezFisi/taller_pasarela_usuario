736d6265dac0ba4359329c7535d7ddb7c5b11b55
Kevin Yzacupe Gomez <softkeyzac@gmail.com>
2016-11-11 02:56:24 -0500

    sufriendo por esta mierda

--- src/main/resources/testdb/schema.sql
+++ src/main/resources/testdb/schema.sql
@@ -1,6 +1,5 @@
 drop table USUARIO if exists;
 
-create table USUARIO (ID bigint identity primary key, NUMBER varchar(9),
-                        NAME varchar(50) not null, BALANCE decimal(8,2), unique(NUMBER));
+create table USUARIO (ID bigint identity primary key, CODIGO varchar(9) not null, NOMBRE varchar(50) not null, CORREO varchar(50),ESCUELA VARCHAR(30) not null, unique(CODIGO));
                         
 ALTER TABLE USUARIO ALTER COLUMN BALANCE SET DEFAULT 0.0;
